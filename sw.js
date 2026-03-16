c3e8260491733735f2289b2ae9547bd9d25d9612
Karandhiver <ai.karandhiver@gmail.com>
2026-03-16 17:48:58 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'tuneflow-v2'
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg']
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return
+  const url = new URL(e.request.url)
+  if (url.hostname !== self.location.hostname) return
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()))
+        return res
+      })
+      .catch(() => caches.match(e.request).then(c => c || caches.match('/index.html')))
+  )
+})
